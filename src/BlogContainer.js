bdc452036b2d04c464d7bbc83906b87e529e1111
Tuitoek <sjtoek@gmail.com>
2022-11-23 17:35:17 +0300

    Proper folder structure

---
+++ src/BlogContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class BlogContainer extends React.Component{
+    render()
+    {return (
+        <div>
+        <h1> Hello from Create React App </h1>
+        <p> I am in a React Component</p>
+        <p>This is awesome</p>
+        <P>So freaking awesome </p>
+        </div>
+    )}
+}
+export default BlogContainer;
